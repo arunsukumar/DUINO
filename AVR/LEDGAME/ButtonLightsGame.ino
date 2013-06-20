00c7fc4bed5f2f35fb776f0e3a4710d1c351c103
Tsvetan Usunov <usunov@olimex.com>
2013-06-20 16:54:43 +0300

    Arduino LEDgame, IR-remote control

---
+++ AVR/LEDGAME/ButtonLightsGame.ino
@@ -0,0 +1,156 @@
+const int buttonPin1 = 0;
+const int buttonPin2 = 1;
+
+int button1State = 0;
+int button2State = 0;
+
+int LightupDelay = 500;
+int DefaultDelay = 500;
+int SpeedChange = 50;
+boolean directionMoving = 0;
+boolean GameEnd = 0;
+
+void setup() {
+  for (int i=0; i<10; i++) {
+  pinMode(i+2, OUTPUT); 
+  }
+  pinMode(buttonPin1, INPUT);   
+  pinMode(buttonPin2, INPUT);
+
+  digitalWrite(2, HIGH);
+  for (int i=0; i<9; i++) {
+  digitalWrite(i+3, LOW); 
+  } 
+}
+
+void loop(){
+
+while (!directionMoving && !GameEnd){
+  for (int i=1; i<11; i++) {
+
+      button1State = HIGH;
+      button2State = HIGH;
+
+    if (i == 10) {
+      for (int j=0; j<5; j++) {
+        digitalWrite(j+2, HIGH);
+      }
+      for (int j=5; j<10; j++) {
+        digitalWrite(j+2, LOW);
+      }
+      GameEnd = 1;
+      break;
+    }
+    
+    digitalWrite(i+2, HIGH);
+    digitalWrite(i+1, LOW);
+    
+    for(int cn=0; cn<LightupDelay; cn+=100) {
+      if (digitalRead(buttonPin1) == LOW) 
+          button1State = LOW;          
+      if (digitalRead(buttonPin2) == LOW) 
+          button2State = LOW;
+      delay(100);
+    }
+  
+    if (button2State == LOW && i != 9) {
+      for (int j=0; j<5; j++) {
+        digitalWrite(j+2, HIGH);
+      }
+      for (int j=5; j<10; j++) {
+        digitalWrite(j+2, LOW);
+      }
+      GameEnd = 1;
+      break;
+    }
+    
+    if (button1State == LOW && i != 0) {
+      for (int j=0; j<5; j++) {
+        digitalWrite(j+2, LOW);
+      }
+      for (int j=5; j<10; j++) {
+        digitalWrite(j+2, HIGH);
+      }
+      GameEnd = 1;
+      break;
+    }
+    
+    if (button2State == LOW && i == 9){
+      LightupDelay -= SpeedChange;
+      directionMoving = 1;
+      break;
+    }
+  }
+}
+
+
+while (directionMoving && !GameEnd){
+  for (int i=11; i>1; i--) {
+
+      button1State = HIGH;
+      button2State = HIGH;
+
+    if (i == 2) {
+      for (int j=0; j<5; j++) {
+        digitalWrite(j+2, LOW);
+      }
+      for (int j=5; j<10; j++) {
+        digitalWrite(j+2, HIGH);
+      }
+      GameEnd = 1;
+      break;
+    }
+    
+    if (i!=2) digitalWrite(i-1, HIGH);
+    digitalWrite(i, LOW);
+    
+    for(int cn=0; cn<LightupDelay; cn+=100) {
+      if (digitalRead(buttonPin1) == LOW) 
+          button1State = LOW;          
+      if (digitalRead(buttonPin2) == LOW) 
+          button2State = LOW;
+      delay(100);
+    }
+  
+    if (button1State == LOW && i != 3) {
+      for (int j=0; j<5; j++) {
+        digitalWrite(j+2, LOW);
+      }
+      for (int j=5; j<10; j++) {
+        digitalWrite(j+2, HIGH);
+      }
+      GameEnd = 1;
+      break;
+    }
+    
+    if (button2State == LOW && i != 11) {
+      for (int j=0; j<5; j++) {
+        digitalWrite(j+2, HIGH);
+      }
+      for (int j=5; j<10; j++) {
+        digitalWrite(j+2, LOW);
+      }
+      GameEnd = 1;
+      break;
+    }
+    
+    if (button1State == LOW && i == 3){
+      LightupDelay -= SpeedChange;
+      directionMoving = 0;
+      break;
+    }
+  }
+}
+
+ if (digitalRead(buttonPin1) == LOW && digitalRead(buttonPin2) == LOW && GameEnd) {
+  LightupDelay = DefaultDelay;
+  directionMoving = 0;
+  digitalWrite(2, HIGH);
+  for (int i=0; i<9; i++) {
+  digitalWrite(i+3, LOW);
+  }
+  GameEnd = 0;
+  delay(1000);
+ }
+}
+
--- AVR/README.md
+++ AVR/README.md
@@ -5,3 +5,7 @@ IR-REMOTE - demo code to control your Arduino project with TV IR remote controll
 SERVO - demo code to control Analog Servo motors like: https://www.olimex.com/Products/RobotParts/ServoMotors/ with AVR-T32U4
 
 MOD-TC-MK2-31885 - demo how to measure temperatures with thermocouple from -250C to +1600C with https://www.olimex.com/Products/Modules/Sensors/MOD-TC-MK2-31855/
+
+LEDGAME - Arduino tutorial for use of buttons and LEDs, simple LED / buttons game
+
+IR-REMOTE - Arduino woring with MOD-IRDA tutorial, TV remote switch ON and OFF LED on breadboard
